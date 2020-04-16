c5601800fb6e77f434f7cd14f12a2d7a7c95b01f
Ignacio <ignaciofrizzera@gmail.com>
2020-04-16 20:17:43 -0300

    Armo grafico, cargo las librerias mediante CDN. Ver bien lo de los arreglos

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
 // Incorpore JQuery para obtener el JSON de la URL
 
-var brazil_info, spain_info, usa_info;
 
-// Todos los arrays van a tener el mismo len, ya que la informacion se toma desde el 22 de enero en adelante
-var arrs_len
+var brazil_confirmed = brazil_deaths = brazil_recovered = [];
+
+var spain_confirmed = spain_deaths = spain_recovered = [];
+
+var usa_confirmed = usa_deaths = usa_recovered = [];
+
+
+// Todas van del 22 de Enero al dia actual
+var dates = [];
+
+// Todos los arrays van a tener el mismo len, ya que la informacion se toma desde el 22 de Enero en adelante
+var arrs_len;
+
+
+// Al cargarse la pagina que ejecute la generacion del grafico
+window.onload = load_data();
 
 /** 
  * Carga y grafica toda la informacion de covid-19 de los paises al abrir la pagina
@@ -19,29 +32,68 @@ function load_data() {
         recovered: 0
      * */
     $.getJSON(url, function (data) {
-        brazil_info = data.Brazil;
+
+        var brazil_info = data.Brazil;
+        var spain_info = data.Spain;
+        var usa_info = data.US;
+
         arrs_len = Object.keys(brazil_info).length;
-
-        spain_info = data.Spain;
-
-        usa_info = data.US;
-
-        // Logeos de testeo para ver bien lo de JSON
-        console.log(brazil_info[0], "Brazil day 0");
-        console.log(spain_info[0], "Spain day 0");
-        console.log(usa_info[0], "USA day 0");
-
-        console.log(brazil_info[0].date, "Brazil day 0 date");
-        console.log(spain_info[0].date, "Spain day 0 date");
-        console.log(usa_info[0].date, "USA day 0 date");
 
         // Test print en parrafo
         var par = document.getElementById("json_plot");
-        par.innerHTML = JSON.stringify(usa_info[0]) + " US day 0";
+        par.innerHTML = JSON.stringify(usa_info[0].date) + " US day 0";
 
+        // Generar arrays
+        for (i = 0; i < arrs_len; i++) {
+
+            brazil_confirmed.push(brazil_info[i].confirmed);
+            usa_confirmed.push(usa_info[i].confirmed);
+            spain_confirmed.push(spain_info[i].confirmed);
+
+            dates.push(brazil_info[i].date);
+        }
+
+
+        // Graficar la informacion
+        grafico = document.getElementById('grafico');
+
+        var brazil_grafico = {
+            x: dates,
+            y: brazil_confirmed,
+            type: 'scatter',
+            name: 'Brasil'
+        };
+
+        var spain_grafico = {
+            x: dates,
+            y: spain_confirmed,
+            type: 'scatter',
+            name: "Espa\361a" //Simbolo de la ñ, no se por qué el UTF-8 no la reconoce
+        };
+
+        var usa_grafico = {
+            x: dates,
+            y: usa_confirmed,
+            type: 'scatter',
+            name: 'U.S.A'
+        };
+
+        var data = [brazil_grafico, spain_grafico, usa_grafico];
+
+        var layout = {
+            xaxis: {
+                title: 'Fecha',
+                showgrid: false,
+                zeroline: false
+            },
+            yaxis: {
+                title: 'Casos confirmados',
+                showline: false
+            }
+        };
+
+        Plotly.newPlot(grafico, data, layout);
     });
-
-    // Faltaria graficar con la informacion obtenida
 }
 
 
